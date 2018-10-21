3cfb27206725ff5e88e432da38240baf0b4dda59
Jakub Filip <kubfil7@gmail.com>
2018-10-22 00:57:14 +0200

    fixes, improvements, some new functions

--- firmware/3d_noughts_and_crosses/3d_noughts_and_crosses/3d_noughts_and_crosses.ino
+++ firmware/3d_noughts_and_crosses/3d_noughts_and_crosses/3d_noughts_and_crosses.ino
@@ -3,11 +3,11 @@
  Created:	10/5/2018 8:30:03 PM
  Author:	morgarath
 */
-
+// #define hide
 
 ////////// MACROS
 //// Time constants
-#define period 600  // period at which to multiplex ledcube layers (ms)
+#define period 2500  // period at which to multiplex ledcube layers (us) // f=400Hz
 
 //// Arduino pins mapping
 #define ARDUINO_PINS
@@ -23,13 +23,13 @@
 #define BUS7Pin 5	// D5  - PC6
 // Arduino pins to select ledcube columms' group to be configured
 #define R0Pin 13	// Red	 collumns 1-8
-#define R1Pin 14	// Red	 collumns 9-16
-#define G0Pin 15	// Green collumns 1-8
-#define G1Pin 16	// Green collumns 9-16
-#define B0Pin 17	// Blue	 collumns 1-8
-#define B1Pin 18	// Blue	 collumns 9-16
+#define R1Pin A0	// Red	 collumns 9-16
+#define G0Pin A1	// Green collumns 1-8
+#define G1Pin A2	// Green collumns 9-16
+#define B0Pin A3	// Blue	 collumns 1-8
+#define B1Pin A4	// Blue	 collumns 9-16
 // Arduino pins to select ledcube layer to be activated
-#define L0Pin 19	// Layer 0
+#define L0Pin A5	// Layer 0
 #define L1Pin 4	    // Layer 1
 #define L2Pin 3	    // Layer 2
 #define L3Pin 2	    // Layer 3
@@ -38,10 +38,10 @@
 #define RXPin 0
 #endif // ARDUINO_PINS
 
-//// AT32u4 port pins
+//// AT32U4 port pins
 #define PORT_PINS
 #ifdef PORT_PINS
-// AT32u4 port pins to configure bus setting selected group of ledcube columns
+// AT32U4 port pins to configure bus setting selected group of ledcube columns
 #define BUS0PPin PORTD6	
 #define BUS1PPin PORTB7	
 #define BUS2PPin PORTB6	
@@ -50,19 +50,19 @@
 #define BUS5PPin PORTE6	
 #define BUS6PPin PORTD7	
 #define BUS7PPin PORTC6	
-// AT32u4 port pins to select ledcube columms' group to be configured
-#define R0PPin PORTC7 // Red	 collumns 1-8
-#define R1PPin PORTF7 // Red	 collumns 9-16
-#define G0PPin PORTF6 // Green   collumns 1-8
-#define G1PPin PORTF5 // Green   collumns 9-16
-#define B0PPin PORTF4 // Blue	 collumns 1-8
-#define B1PPin PORTF1 // Blue	 collumns 9-16
-// AT32u4 port pins to select ledcube layer to be activated
+// AT32U4 port pins to select ledcube columms' group to be configured
+#define R0PPin PORTC7 // Red collumns 1-8
+#define R1PPin PORTF7 // Red collumns 9-16
+#define G0PPin PORTF6 // Green collumns 1-8
+#define G1PPin PORTF5 // Green collumns 9-16
+#define B0PPin PORTF4 // Blue collumns 1-8
+#define B1PPin PORTF1 // Blue collumns 9-16
+// AT32U4 port pins to select ledcube layer to be activated
 #define L0PPin PORTF0 // Layer 0
 #define L1PPin PORTD4 // Layer 1
 #define L2PPin PORTD0 // Layer 2
 #define L3PPin PORTD1 // Layer 3
-// AT32u4 port pins to communicate Bluetooth module over UART
+// AT32U4 port pins to communicate Bluetooth module over UART
 #define TXPPin PORTD3; 
 #define RXPPin PORTD2; 
 #endif // PORT_PINS
@@ -70,7 +70,7 @@
 //// Ports of every pin
 #define PORTS
 #ifdef PORTS
-// AT32u4 ports to configure bus setting selected group of ledcube columns
+// AT32U4 ports to configure bus setting selected group of ledcube columns
 #define BUS0Port PORTD	
 #define BUS1Port PORTB	
 #define BUS2Port PORTB	
@@ -79,19 +79,19 @@
 #define BUS5Port PORTE	
 #define BUS6Port PORTD	
 #define BUS7Port PORTC	
-// AT32u4 ports to select ledcube columms' group to be configured
-#define R0Port PORTC // Red	 collumns 1-8
-#define R1Port PORTF // Red	 collumns 9-16
-#define G0Port PORTF // Green   collumns 1-8
-#define G1Port PORTF // Green   collumns 9-16
-#define B0Port PORTF // Blue	 collumns 1-8
-#define B1Port PORTF // Blue	 collumns 9-16
-// AT32u4 ports to select ledcube layer to be activated
+// AT32U4 ports to select ledcube columms' group to be configured
+#define R0Port PORTC // Red	collumns 1-8
+#define R1Port PORTF // Red	collumns 9-16
+#define G0Port PORTF // Green collumns 1-8
+#define G1Port PORTF // Green collumns 9-16
+#define B0Port PORTF // Blue collumns 1-8
+#define B1Port PORTF // Blue collumns 9-16
+// AT32U4 ports to select ledcube layer to be activated
 #define L0Port PORTF // Layer 0
 #define L1Port PORTD // Layer 1
 #define L2Port PORTD // Layer 2
 #define L3Port PORTD // Layer 3
-// AT32u4 ports to communicate Bluetooth module over UART
+// AT32U4 ports to communicate Bluetooth module over UART
 #define TXPort PORTD; 
 #define RXPort PORTD; 
 #endif // PORTS
@@ -101,33 +101,58 @@
 #define GREEN 1
 #define BLUE 2
 
+#define LAYER0 0
+#define LAYER1 1
+#define LAYER2 2
+#define LAYER3 3
+
+#define COLUMN0 0
+#define COLUMN1 1
+#define COLUMN2 2
+#define COLUMN3 3
+#define COLUMN4 4
+#define COLUMN5 5
+#define COLUMN6 6
+#define COLUMN7 7
+#define COLUMN8 8
+#define COLUMN9 9
+#define COLUMN10 10
+#define COLUMN11 11
+#define COLUMN12 12
+#define COLUMN13 13
+#define COLUMN14 14
+#define COLUMN15 15
+
+
+
+#define LAYERS_NUM 4
+
 ////////// VARIABLES
-uint32_t currentMillis;
-uint32_t previousMillis = 0;
-uint8_t layer = 0x00;
-bool layer0Changed = false;
-bool layer1Changed = false;
-bool layer2Changed = false;
-bool layer3Changed = false;
-uint16_t buffer = 0x0000;
-//                 Columns:  red     green   blue       Layer:
-uint16_t ledcube[4][3] = { { 0x1000, 0x0100, 0x0010},	// 0
-                           { 0x0100, 0x0010, 0x0001},   // 1
-                           { 0x0010, 0x0001, 0x1000},	// 2
-                           { 0x0001, 0x1000, 0x0100} };	// 3
+uint32_t current_micros;
+uint32_t previous_micros = 0;
+uint8_t active_layer = 0x00;
+uint8_t column_to_set = 0;
+uint8_t layer_to_set = 0;
+uint8_t color_to_set = 0;
 
+//                 Columns:  red:    green:  blue:      Layer:
+uint16_t led_cube[4][3] = { {0x0000, 0x0000, 0x0000},	// 0
+                           { 0x0000, 0x0000, 0x0000},   // 1
+                           { 0x0000, 0x0000, 0x0000},	// 2
+                           { 0x0000, 0x0000, 0x0000} };	// 3
 
-////////// FUNCTION DECLARATIONS
-inline uint8_t modifyBit(uint8_t operand, uint8_t pos, uint8_t bit);
-inline  uint8_t activateLayer(uint8_t operand, uint8_t pos);
-inline  uint8_t deactivateLayer(uint8_t operand, uint8_t pos);
-void setBus(uint8_t buffer);
-void setColumns(void);
-
+// DEBUGGING VARIABLES
+uint8_t a = 1;
+uint8_t b = 0;
+long tic;
+long toc;
+long interval;
+uint8_t buffer;
+uint16_t subperiod = 0;
 
 ////////// INITIALIZATON
 void setup()
-{
+{   
     pinMode(BUS0Pin, OUTPUT);
     pinMode(BUS1Pin, OUTPUT);
     pinMode(BUS2Pin, OUTPUT);
@@ -163,110 +188,197 @@ void setup()
     digitalWrite(G1Pin, LOW);
     digitalWrite(B0Pin, LOW);
     digitalWrite(B1Pin, LOW);
+
+    Serial.begin(115200);
 }
 
 
-////////// CONTINUOUS TASKS
+////////// TASKS
 void loop()
 {
-    currentMillis = millis();
+    current_micros = micros();
 
-    if (currentMillis - previousMillis >= period)
+    if (current_micros - previous_micros >= period)
     {
-        previousMillis = currentMillis;
-        switch (layer)
+        previous_micros = current_micros;
+        subperiod++;
+        #ifndef hide
+
+        switch (active_layer)
         {
-        case 0:
-            deactivateLayer(L3Port, L3PPin);
-            activateLayer(L0Port, L0PPin);
-            if (layer1Changed)
-            {
-                setLayer(layer+1);
-                layer0Changed = false;
-            }
+        case LAYER0:
+            clrPortBit(&L3Port, L3PPin);
+            setLayer(active_layer);
+            setPortBit(&L0Port, L0PPin);
             break;
 
-        case 1:
-            deactivateLayer(L0Port, L0PPin);
-            activateLayer(L1Port, L1PPin);
-            if (layer2Changed)
-            {
-                setLayer(layer+1);
-                layer1Changed = false;
-            }
+        case LAYER1:
+            clrPortBit(&L0Port, L0PPin);
+            setLayer(active_layer);
+            setPortBit(&L1Port, L1PPin);
             break;
 
-        case 2:
-            deactivateLayer(L1Port, L1PPin);
-            activateLayer(L2Port, L2PPin);
-            if (layer3Changed)
-            {
-                setLayer(layer+1);
-                layer2Changed = false;
-            }
+        case LAYER2:
+            clrPortBit(&L1Port, L1PPin);
+            setLayer(active_layer);
+            setPortBit(&L2Port, L2PPin);
             break;
 
-        case 3:
-            deactivateLayer(L2Port, L2PPin);
-            activateLayer(L3Port, L3PPin);
-            if (layer0Changed)
-            {
-                setLayer(layer+1);
-                layer3Changed = false;
-            }
+        case LAYER3:
+            clrPortBit(&L2Port, L2PPin);
+            setLayer(active_layer);
+            setPortBit(&L3Port, L3PPin);
             break;
 
         default:
             break;
         }
 
-        if (layer++ > 3)
+        if (++active_layer >= LAYERS_NUM)
         {
-            layer = 0;
+            active_layer = 0;
         }
-    }
-}
 
+        #endif // !hide
+
+        testLedCube();
+
+        // Testing space
+        #ifdef hide
+        if (a)
+        {
+            clrPortBit(&L1Port, L1PPin);
+            setPortBit(&L0Port, L0PPin);
+            buffer = 0xFF;
+            tic = micros();
+            setLayer(0);
+            toc = micros();
+        }
+        if (b)
+        {
+            clrPortBit(&L0Port, L0PPin);
+            setPortBit(&L1Port, L1PPin);
+            buffer = 0x00;
+            tic = micros();
+            setLayer(1);
+            toc = micros();
+        }
+        if (a)
+        {
+            a = 0;
+            b = 1;
+        }
+        else
+        {
+            a = 1;
+            b = 0;
+        }
+        #endif // hide
+    }
+
+
+}
 
 ////////// FUNCTION DEFINITIONS
-inline uint8_t modifyBit(uint8_t operand, uint8_t pos, uint8_t bit)
+void modifyPortBit(volatile uint8_t *port, uint8_t bit_pos, uint8_t val)
 {
-    return operand = (operand & ~(1 << pos)) | ((bit << pos)&(1 << pos));
+     *port = (*port & ~(1 << bit_pos)) | ((val << bit_pos)&(1 << bit_pos));
 }
 
-inline  uint8_t setBit(uint8_t operand, uint8_t pos)
+void setPortBit(volatile uint8_t *port, uint8_t bit_pos)
 {
-    return operand |= (1 << pos);
+    *port |= (1 << bit_pos);
 }
 
-inline  uint8_t clrBit(uint8_t operand, uint8_t pos)
+void clrPortBit(volatile uint8_t *port, uint8_t bit_pos)
 {
-    return operand &= !(1 << pos);
-}
- 
-inline uint8_t activateLayer(uint8_t operand, uint8_t pos)
-{
-    return setBit(operand, pos);
-}
-
-inline uint8_t deactivateLayer(uint8_t operand, uint8_t pos)
-{
-    return clrBit(operand, pos);
+    *port &= !(1 << bit_pos);
 }
 
 void setBus(uint8_t buffer)
 {
-    modifyBit(BUS0Port, BUS0PPin,  (buffer       & 0xFE)); 
-    modifyBit(BUS1Port, BUS1PPin, ((buffer >> 1) & 0xFE)); 
-    modifyBit(BUS2Port, BUS2PPin, ((buffer >> 2) & 0xFE)); 
-    modifyBit(BUS3Port, BUS3PPin, ((buffer >> 3) & 0xFE)); 
-    modifyBit(BUS4Port, BUS4PPin, ((buffer >> 4) & 0xFE)); 
-    modifyBit(BUS5Port, BUS5PPin, ((buffer >> 5) & 0xFE)); 
-    modifyBit(BUS6Port, BUS6PPin, ((buffer >> 6) & 0xFE)); 
-    modifyBit(BUS7Port, BUS7PPin, ((buffer >> 7) & 0xFE)); 
+    modifyPortBit(&BUS0Port, BUS0PPin,  (buffer       & 0x01)); 
+    modifyPortBit(&BUS1Port, BUS1PPin, ((buffer >> 1) & 0x01)); 
+    modifyPortBit(&BUS2Port, BUS2PPin, ((buffer >> 2) & 0x01)); 
+    modifyPortBit(&BUS3Port, BUS3PPin, ((buffer >> 3) & 0x01)); 
+    modifyPortBit(&BUS4Port, BUS4PPin, ((buffer >> 4) & 0x01)); 
+    modifyPortBit(&BUS5Port, BUS5PPin, ((buffer >> 5) & 0x01)); 
+    modifyPortBit(&BUS6Port, BUS6PPin, ((buffer >> 6) & 0x01)); 
+    modifyPortBit(&BUS7Port, BUS7PPin, ((buffer >> 7) & 0x01));
 }
 
-uint8_t setLayer(uint8_t layer)
+void setLayer(uint8_t layer)
+{   
+    modifyPortBit(&R0Port, R0PPin, HIGH);
+    setBus(led_cube[layer][RED] & 0xFF);
+    modifyPortBit(&R0Port, R0PPin, LOW);
+    modifyPortBit(&R1Port, R1PPin, HIGH);
+    setBus(led_cube[layer][RED] >> 8);
+    modifyPortBit(&R1Port, R1PPin, LOW);
+    modifyPortBit(&G0Port, G0PPin, HIGH);
+    setBus(led_cube[layer][GREEN] & 0xFF);
+    modifyPortBit(&G0Port, G0PPin, LOW);
+    modifyPortBit(&G1Port, G1PPin, HIGH);
+    setBus(led_cube[layer][GREEN] >> 8);
+    modifyPortBit(&G1Port, G1PPin, LOW);
+    modifyPortBit(&B0Port, B0PPin, HIGH);
+    setBus(led_cube[layer][BLUE] & 0xFF);
+    modifyPortBit(&B0Port, B0PPin, LOW);
+    modifyPortBit(&B1Port, B1PPin, HIGH);
+    setBus(led_cube[layer][BLUE] >> 8);
+    modifyPortBit(&B1Port, B1PPin, LOW);
+}
+
+void updateLedCube(uint8_t layer, uint8_t column, uint8_t color)
 {
-    return;
+    led_cube[layer][color] |= 1 << column;
+}
+
+
+bool isAvailable(uint8_t layer, uint8_t column)
+{   
+    uint32_t led;
+    for (int color = 0; color < 3; color++)
+    {
+        led = (led_cube[layer][color] >> column) & 0x01;
+        if (led == 1)
+        {
+            return false;
+        }
+    }    
+    return true;
+}
+
+void testLedCube(void)
+{
+    if (subperiod > 10)
+    {
+        if (isAvailable(layer_to_set, column_to_set))
+        {
+            updateLedCube(layer_to_set, column_to_set, color_to_set);
+        }
+        column_to_set++;
+        if (layer_to_set == 4)
+        {
+            layer_to_set = 0;
+            color_to_set++;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    led_cube[i][j] = 0x0000;
+                }
+            }
+        }
+        if (column_to_set == 16)
+        {
+            column_to_set = 0;
+            layer_to_set++;
+        }
+        if (color_to_set == 3)
+        {
+            color_to_set = 0;
+        }
+        subperiod = 0;
+    }
 }
